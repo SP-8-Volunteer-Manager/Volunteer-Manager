5e591a12ed9b601cdd0c077ec447f58ad0852de8
nino <93230927+ninuljaja@users.noreply.github.com>
2024-10-22 01:46:55 -0400

    Implemented backend for contactUs form

--- Frontend/src/Components/StateDropdown.jsx
+++ Frontend/src/Components/StateDropdown.jsx
@@ -4,7 +4,7 @@ function StateDropdown() {
   const [selectedState, setSelectedState] = useState('');
   const states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
   return (
-    <select value={selectedState} onChange={(e) => setSelectedState(e.target.value)}>
+    <select className="form-select" id="inputState" value={selectedState} onChange={(e) => setSelectedState(e.target.value)}>
       <option value="">Select State</option>
       {states.map((state) => (
         <option key={state} value={state}>
