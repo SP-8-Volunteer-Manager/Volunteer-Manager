ec49352bf5a874f1525f6f1d7a6e76adc16ad1a0
Avinash <avidefx@gmail.com>
2024-10-25 17:43:42 -0400

    Change routine as prop

--- Frontend/src/Components/CarrierDropdown.jsx
+++ Frontend/src/Components/CarrierDropdown.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
-
-function CarrierDropdown() {
-  const [selectedCarrier, setSelectedCarrier] = useState('');
+function CarrierDropdown({dropdownChange}) {
   const carriers = ["AT&T", "Boost Mobile", "Cricket Wireless", "Consumer Cellular", "FreedomPop", "H2O Wireless", "Mint Mobile", "Metro by T-Mobile", "Republic Wireless", "Simple Mobile", "Sprint", "Straight Talk", "T-Mobile", "Ting", "TracFone", "US Cellular", "Verizon", "Virgin Mobile", "Xfinity Mobile"];
 
   return (
-    <select className="form-select" value={selectedCarrier} onChange={(e) => setSelectedCarrier(e.target.value)}>
+    <select className="form-select" name="carrier" onChange={dropdownChange}>
       <option value="">Select Carrier</option>
       {carriers.map((c) => (
         <option key={c} value={c}>
