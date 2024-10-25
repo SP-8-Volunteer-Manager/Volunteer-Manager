d0e8c9ea6682b5942f1ac7211ff3ec32af793b43
nino <93230927+ninuljaja@users.noreply.github.com>
2024-10-24 20:59:40 -0400

    forgotpassword

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -9,4 +9,7 @@ router.post('/signup', authController.signup);
 // Route for user login
 router.post('/login', authController.login);
 
+// Route for password reset
+router.post('/forgot-password', authController.resetPassword)
+
 module.exports = router; //  sure to export the router
