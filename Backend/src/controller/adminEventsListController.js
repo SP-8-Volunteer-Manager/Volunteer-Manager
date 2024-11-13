8d53402a9660ca463219ac128b00a1097280a770
Avinash <avidefx@gmail.com>
2024-11-13 16:05:32 -0500

    Implemented Show All in Events List, Prevent event list referesh if no event created in the modal.

--- Backend/src/controller/adminEventsListController.js
+++ Backend/src/controller/adminEventsListController.js
@@ -1,17 +1,26 @@
 const pool = require('../config/supabaseClient');
 
 
+//This method will return events greater than or equal today by default. 
+//If the showall parameter is true it will return events from one month before onwards
 const getEventLists = async (req, res) => {
     try {
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
+        const { showAll } = req.params;
+        const cutoffdate = new Date();
+        if (showAll == "undefined" || showAll == "true")
+        {
+            //console.log("Set cutoff date to one month back")
+            cutoffdate.setMonth(cutoffdate.getMonth() - 1);
+        }
+       
+        cutoffdate.setHours(0, 0, 0, 0);
         const { data, error } = await pool
             .from('task')
             .select(`*,
                 assignment(volunteer(first_name, last_name)),
                 task_type(type_name)
                 `)
-                .gte('start_date', today.toLocaleString())
+                .gte('start_date', cutoffdate.toLocaleString())
                 .order('start_date', { ascending: true })
                 .order('start_time', { ascending: true });
                 //console.log(data)
